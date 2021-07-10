71ef09408ff2d7b7698849e6cbc9679ab2434092
Carlos Encine <contato@carlosencine.com>
2021-07-10 10:19:12 -0300

    add sendGrid to send weather emails

--- Services/SendMessage.cs
+++ Services/SendMessage.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using WeatherToEmail.Weather;
 
 namespace WeatherToEmail.Services
@@ -9,49 +11,74 @@ namespace WeatherToEmail.Services
     {
         private string MailFrom { get; set; }
         private string MailTo { get; set; }
-        private string MailServer { get; set; }
-        private string MailPass { get; set; }
-
+        public string ApiKey { get; set; }
+        
         public SendMessage()
         {
             MailFrom = Environment.GetEnvironmentVariable("mail_from");
             MailTo = Environment.GetEnvironmentVariable("mail_to");
-            MailServer = Environment.GetEnvironmentVariable("mail_smtp");
-            MailPass = Environment.GetEnvironmentVariable("mail_pass");
+            ApiKey = Environment.GetEnvironmentVariable("SENDGRID_KEY");
         }
 
 
-        public void SendMessageToUser(WeatherResult weather)
+        public async void SendMessageToUser(WeatherResult weather)
         {
-            using (var mailClient = new SmtpClient(MailServer))
+            try
             {
-                var basicCredentials = new NetworkCredential(MailFrom, MailPass);
-                using (var message = new MailMessage())
-                {
-                    MailAddress from = new MailAddress(MailFrom, "Carlos Encine");
-                    MailAddress to = new MailAddress(MailTo, "destination");
+                var client = new SendGridClient(ApiKey);
+                var from = new EmailAddress(MailFrom, "Weather Now");
 
-                    mailClient.Host = MailServer;
-                    mailClient.UseDefaultCredentials = false;
-                    mailClient.Credentials = basicCredentials;
+                var weatherDate = Convert.ToDateTime(weather.Data.Date);
 
-                    var weatherDate = Convert.ToDateTime(weather.Data.Date);
+                var subject = $"Clima agora: {weatherDate:dd/MM/yyyy hh:mm}";
+                var to = new EmailAddress(MailTo);
+                var plainTextContent = $"Tempo na cidade de {weather.Name} - {weather.State}\n" +
+                                       $"----------------------------------------------------------------------\n" +
+                                       $"Temperatura: {weather.Data.Temperature}°C\n" +
+                                       $"Humidade: {weather.Data.Humidity}%\n" +
+                                       $"Condição: {weather.Data.Condition}\n" +
+                                       $"Sensação Termica: {weather.Data.Sensation}°C";
 
-                    message.From = from;
-                    message.To.Add(to);
-                    message.Subject = $"Clima agora: {weatherDate:dd/MM/yyyy hh:mm}";
-
-                    string clima = $"Tempo na cidade de {weather.Name} - {weather.State}\n" +
-                                   $"----------------------------------------------------------------------\n" +
-                                   $"Temperatura: {weather.Data.Temperature}°C\n" +
-                                   $"Humidade: {weather.Data.Humidity}%\n" +
-                                   $"Condição: {weather.Data.Condition}\n" +
-                                   $"Sensação Termica: {weather.Data.Sensation}°C";
-
-                    message.Body = clima;
-                    mailClient.Send(message);
-                }
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, string.Empty);
+                await client.SendEmailAsync(msg);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+
+            //using (var mailClient = new SmtpClient(MailServer))
+            //{
+            //    var basicCredentials = new NetworkCredential(MailFrom, MailPass);
+            //    using (var message = new MailMessage())
+            //    {
+            //        MailAddress from = new MailAddress(MailFrom, "Carlos Encine");
+            //        MailAddress to = new MailAddress(MailTo, "destination");
+
+            //        mailClient.Host = MailServer;
+            //        mailClient.UseDefaultCredentials = false;
+            //        mailClient.Credentials = basicCredentials;
+
+            //        var weatherDate = Convert.ToDateTime(weather.Data.Date);
+
+            //        message.From = from;
+            //        message.To.Add(to);
+            //        message.Subject = $"Clima agora: {weatherDate:dd/MM/yyyy hh:mm}";
+
+            //        string clima = $"Tempo na cidade de {weather.Name} - {weather.State}\n" +
+            //                       $"----------------------------------------------------------------------\n" +
+            //                       $"Temperatura: {weather.Data.Temperature}°C\n" +
+            //                       $"Humidade: {weather.Data.Humidity}%\n" +
+            //                       $"Condição: {weather.Data.Condition}\n" +
+            //                       $"Sensação Termica: {weather.Data.Sensation}°C";
+
+            //        message.Body = clima;
+            //        mailClient.Send(message);
+            //    }
+        }
         }
     }
 }
